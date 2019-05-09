6cf9da8331271465a62f2fa352c35bc779ceb9a2
Thanos <thanoskapsalis97@gmail.com>
2019-05-09 13:17:26 +0300

    ok

--- Client/src/Booking.java
+++
@@ -1,9 +0,0 @@
-
-import java.rmi.*;
-
-public interface Booking extends Remote {
-
-    public void BookChecker(String departure, String destination, String depart_date, String arrival_date, int passengers,int token) throws RemoteException;
-
-    public boolean Confirn() throws RemoteException;
-}
