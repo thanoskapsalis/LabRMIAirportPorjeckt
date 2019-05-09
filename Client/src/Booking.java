f56537f4c10e5d9f5e5604942846018a794c6fcc
Thanos <nicklm1997@gmail.com>
2019-05-09 12:14:53 +0300

    RandomInput

--- Client/src/Booking.java
+++ Client/src/Booking.java
@@ -3,7 +3,7 @@ import java.rmi.*;
 
 public interface Booking extends Remote {
 
-    public void BookChecker(String departure, String destination, String depart_date, String arrival_date, int passengers) throws RemoteException;
+    public void BookChecker(String departure, String destination, String depart_date, String arrival_date, int passengers,int token) throws RemoteException;
 
     public boolean Confirn() throws RemoteException;
 }
